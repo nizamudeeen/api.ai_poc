05bf6c7f9a350c658fc55209419907453aac63dc
nizamudeeen <nizamudeeen@gmail.com>
2016-08-10 23:39:39 +0530

    reverting the changes

--- app.py
+++ app.py
@@ -29,9 +29,14 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "planAlertFinder":
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-   result = "enroled in rppm and prism"
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+    result = urllib.urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
