ef9dd6dcafbc826bdd9a62295f1669a74d8b1aae
nizamudeeen <nizamudeeen@gmail.com>
2016-08-11 17:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,6 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "planAlertFinder":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-   
     result = "Hello"
     data = json.loads(result)
     res = makeWebhookResult(data)
